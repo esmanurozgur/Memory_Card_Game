e2fd72bdefe18d67d74bdf6fa5b1035fdd855b72
Esmanur Özgür <esmanurozgr41@gmail.com>
2026-04-23 15:45:55 +0300

    SDL2 kütüphanesi eklendi ve test penceresi çalıştırıldı

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc main.c -o memory_card_game
+	gcc main.c -I./SDL2/include/SDL2 -L./SDL2/lib -lmingw32 -lSDL2main -lSDL2 -o memory_card_game
--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
-int main() {
-    printf("Memory Card Game Is Starting... \n");
+#include <SDL.h>
+
+int main(int argc, char *argv[]) {
+    
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL Baslatilamadi! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Pencere olustur
+    SDL_Window *window = SDL_CreateWindow("Memory Card Game", 
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          800, 600, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Pencere olusturulamadi! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Ekranda 3000 milisaniye (3 saniye) bekle
+    SDL_Delay(3000);
+
+    // Hafizayi temizle ve cik
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
