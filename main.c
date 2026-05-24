354d417ac82a0a303146661bb2b6d75f9c749e7b
Esmanur Özgür <esmanurozgr41@gmail.com>
2026-05-24 15:44:23 +0300

    Ekran 1024x768 olarak genisletildi, giris ekranı ve yeniden baslat butonu eklendi.

--- main.c
+++ main.c
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
     SDL_Window *window = SDL_CreateWindow("Memory Card Game", 
                                           SDL_WINDOWPOS_CENTERED, 
                                           SDL_WINDOWPOS_CENTERED, 
-                                          800, 600, SDL_WINDOW_SHOWN);
+                                          1024, 768, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         printf("Pencere olusturulamadi! Hata: %s\n", SDL_GetError()); //SDL_GetError() fonksiyonu en son olusan hatayi string olarak dondurur
         SDL_Quit(); // SDL_Init() ile baslatilan subsistemleri kapatir
@@ -102,9 +102,13 @@ int main(int argc, char *argv[])
     cardTexture[7]= LoadTexture("assets/php.png", renderer);
     cardTexture[8]= LoadTexture("assets/swift.png", renderer);
     
+
+    int gameMode=0; //0: menu, 1: oyun, 2: oyun bitti
     int isRunning = 1; //oyunun calismaya devam edip etmeyecegni kontrol eden yapidir, 1 ise calisiyor, 0 ise duracak
     SDL_Event event; // klavye, fare veya pencere olaylarini (event) tutar
 
+    int screenW= 1024; //ekran genisligi
+    int screenH= 768;  //ekran yuksekligi
     int cardWidth = 100;  // Kart genisligi
     int cardHeight = 100; // Kart yuksekligi
     int padding = 20;     // Kartlar arasi bosluk
@@ -113,14 +117,30 @@ int main(int argc, char *argv[])
 
     // Ekranin (800x600) tam ortasina hizalamak icin baslangic noktalari secerken
     //4x4 lük sistemimizde 4 kart genisligi ve 3 tane de padding var
-    //ekrangenisligi - (4*kartgenisligi + 3*padding) formulunu x'in baslangic noktasini bulmak icin kullaniriz
-    //ekran yuksekligi - (4*kartyuksekligi + 3*padding) formulunu y'nin baslangic noktasini bulmak icin kullaniriz
-    int startX = 170; // 800 - (4*100 + 3*20) = 800 - 460 = 170
-    int startY = 70;  // 600 - (4*100 + 3*20) = 600 - 460 = 70
+    //startX = (ekran genisligi - (4 kart genisligi + 3 padding)) / 2
+    //startY = (ekran yuksekligi - (4 kart yuksekligi + 3 padding)) / 2
+    int startX = (screenW - (4 * cardWidth + 3 * padding)) / 2; //kartlarin baslangic x konumu
+    int startY = (screenH - (4 * cardHeight + 3 * padding)) / 2;  //kartlarin baslangic y konumu
+
 
 
 
     Uint32 startTime = SDL_GetTicks(); //oyuna basladıgımız anı milisaniye cinsinden verir
+    
+    //Butonlarin yerleri
+    SDL_Rect startButton = {
+        .x = 412, //start butonunun x konumu
+        .y = 350, //start butonunun y konumu
+        .w = 200, //start butonunun genisligi
+        .h = 60  //start butonunun yuksekligi
+    };
+    SDL_Rect restartButton={
+        .x = 412, //restart butonunun x konumu
+        .y = 650, //restart butonunun y konumu
+        .w = 200, //restart butonunun genisligi
+        .h = 60   //restart butonunun yuksekligi
+    };
+
 
     while (isRunning)
     {
@@ -161,31 +181,59 @@ int main(int argc, char *argv[])
                     int mouseX = event.button.x; //fare tiklamasinin x koordinati
                     int mouseY = event.button.y; //fare tiklamasinin y koordinati
 
-                    //tiklanan kartin hangi satir ve sutunda oldugunu bulmak icin
-                    int col = (mouseX - startX) / (cardWidth + padding);
-                    int row = (mouseY - startY) / (cardHeight + padding);
+                    //durum 0:Giris ekrani
+                    if(gameMode == 0) {
+                        if(mouseX >= startButton.x && mouseX <= startButton.x + startButton.w &&
+                           mouseY >= startButton.y && mouseY <= startButton.y + startButton.h) {
+                            //tiklanan x kutunun sol kenarından buyuk ve sag kenarindan kucuk
+                            //tiklanan y kutunun ust kenarindan buyuk ve alt kenarindan kucukse
+                            //start butonuna tiklandi, oyunu baslat
+                            gameMode = 1; //oyun moduna gectik, artik kartlar ekranda olacak ve tiklanabilir olacaklar
+                            startTime = SDL_GetTicks(); //oyunu baslattigimiz anin zamanini kaydedelim, bu zamani oyun sure hesaplamak icin kullanacagiz
+                            printf("Oyun baslatildi!\n");
+                        }
+                    }
 
-                    //tiklanan yer gecerli bi satir ve sutunda mi kontrolu
-                    if (col >= 0 && col < COLS && row >= 0 && row < ROWS) {
+                    //durum 1: Oyun ekrani, kartlar tiklanabilir
+                    else if(gameMode == 1 && game.isGameOver == 0) {
+                        //tiklanan kartin hangi satir ve sutunda oldugunu bulmak icin
+                        int col = (mouseX - startX) / (cardWidth + padding);
+                        int row = (mouseY - startY) / (cardHeight + padding);
 
-                         if(game.board[row][col].state == 0 && game.flippedCount < 2) { //kart kapaliysa ve 2 karttan az acik varsa
-                            game.board[row][col].state = 1; //kart acilir
-                         }
+                        //tiklanan yer gecerli bi satir ve sutunda mi kontrolu
+                        if (col >= 0 && col < COLS && row >= 0 && row < ROWS) {
 
-                        if(game.flippedCount == 0) {
-                            //acilan ilk kartin konumunu kaydettim
-                            game.firstRow = row;
-                            game.firstCol = col;
-                            game.flippedCount = 1; //bir kart acildi
-                         } 
+                            if(game.board[row][col].state == 0 && game.flippedCount < 2) { //kart kapaliysa ve 2 karttan az acik varsa
+                                game.board[row][col].state = 1; //kart acilir
+                            }
+
+                            if(game.flippedCount == 0) {
+                                //acilan ilk kartin konumunu kaydettim
+                                game.firstRow = row;
+                                game.firstCol = col;
+                                game.flippedCount = 1; //bir kart acildi
+                            } 
                         
-                        else if(game.flippedCount == 1) {
-                            //acilan ikinci kartin konumunu kaydettim
-                            game.secondRow = row;
-                            game.secondCol = col;
-                            game.flippedCount = 2; //iki kart acildi
-                            game.moves++; //kullanici bir hamle yapti, moves sayisini arttir
-                            printf("\nHamle sayisi: %d\n", game.moves);
+                            else if(game.flippedCount == 1) {
+                                //acilan ikinci kartin konumunu kaydettim
+                                game.secondRow = row;
+                                game.secondCol = col;
+                                game.flippedCount = 2; //iki kart acildi
+                                game.moves++; //kullanici bir hamle yapti, moves sayisini arttir
+                                printf("\nHamle sayisi: %d\n", game.moves);
+                            }
+                        }
+                    }
+                     //durum 2: Oyun bitti, restart butonu tiklanabilir
+                    else if(game.isGameOver == 1) {
+                        if(mouseX >= restartButton.x && mouseX <= restartButton.x + restartButton.w &&
+                           mouseY >= restartButton.y && mouseY <= restartButton.y + restartButton.h) {
+                            //tiklanan x kutunun sol kenarından buyuk ve sag kenarindan kucuk
+                            //tiklanan y kutunun ust kenarindan buyuk ve alt kenarindan kucukse
+                            //restart butonuna tiklandi, oyunu yeniden baslat
+                            initGame(&game); //oyunu baslatan fonksiyonu tekrar cagirarak oyunu sifirla
+                            startTime = SDL_GetTicks(); //yeni bir oyun baslattigimiz icin zamani sifirla
+                            printf("Oyun yeniden baslatildi!\n");
                         }
                     }
                 }
@@ -197,74 +245,121 @@ int main(int argc, char *argv[])
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // rgba degeriyle rengimiz siyah ve tam opak 
         SDL_RenderClear(renderer); //renderirin tamamini temizler ve setRenderDrawColor() ile belirlenen renk ile doldurur
 
-    
-        
+        if(gameMode == 0) {
+            //giris ekranini cizdir
+            SDL_SetRenderDrawColor(renderer, 50, 50, 150, 255); // Koyu mavi arkaplan
+            SDL_RenderClear(renderer); //renderirin tamamini temizler ve setRenderDrawColor() ile belirlenen renk ile doldurur
+            
+            //basla butonu
+            SDL_SetRenderDrawColor(renderer, 0 ,200, 0 ,255); // Yesil renk
+            SDL_RenderFillRect(renderer, &startButton); //start butonunu cizdir
 
-        
-     
-
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                // SDL_Rect, bir dikdortgenin nerede cizilecegini (x,y) koordinatlarini ve boyutunu (w,h) weight height tutan bir yapidir
-                SDL_Rect rect;
-
-                rect.x = startX + c * (cardWidth + padding); //170 +sutun*120
-
-                rect.y = startY + r * (cardHeight + padding); //70 + satir*120
-
-                rect.w = cardWidth;
-
-                rect.h = cardHeight;
-
-
-                if(game.board[r][c].state == 0) {
-                    SDL_SetRenderDrawColor(renderer,118, 206, 242, 255); // Kapali kartlar icin acik mavi
-                    SDL_RenderFillRect(renderer, &rect); // Dikdortgenin icini boyayarak ekrana cizdirir} 
-                }
-                else if(game.board[r][c].state == 1 || game.board[r][c].state == 2) 
-                {
-                    int val= game.board[r][c].val; //kart degeri, 1'den 8'e kadar
-
-                    if(cardTexture[val] != NULL) {
-                        //kart degerine karslik gelen texture'i cizdirir
-                        SDL_RenderCopy(renderer, cardTexture[val], NULL, &rect); //kart degerine karslik gelen texture'i cizdirir
-                    }
-                    else {
-                        //eger texture yuklenemedi ise, karti beyaz olarak cizdirir
-                        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Beyaz renk
-                        SDL_RenderFillRect(renderer, &rect); // Dikdortgenin icini boyayarak ekrana cizdirir
-                    }
-                }
-            }
-        }
-
-        if(font != NULL){
-            char timeText[100];
-
-            if(game.matchedPairs == 8) {
-                snprintf(timeText, sizeof(timeText), "Tebrikler! Tum kartlari eslestirdiniz. Kalan sure: %d saniye", game.timeRemaining);
-            }
-            else if(game.timeRemaining <= 0) {
-                snprintf(timeText, sizeof(timeText), "SURE BITTI! Eslesen kart ciftleri: %d", game.matchedPairs);
-            }
-            else {
-                snprintf(timeText, sizeof(timeText), "Kalan Sure: %d saniye | Hamle: %d | Eslesen Ciftler: %d", game.timeRemaining, game.moves, game.matchedPairs);
-            }
-
-            SDL_Color textColor = {255, 255, 255}; // Beyaz renk
-            SDL_Surface* textSurface = TTF_RenderText_Solid(font, timeText, textColor); //yaziyi bir surface'e renderlar
+            //start butonu yazisi
+            SDL_Color white={255, 255, 255}; 
+            SDL_Surface* textSurface = TTF_RenderText_Solid(font, "Oyunu Baslat", white); //buton yazisini bir surface'e renderlar
             SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface); //surface'i texture'a cevirir
-
             SDL_Rect textRect;
-            textRect.x = 20; //yazinin x konumu
-            textRect.y = 20; //yazinin y konumu
+            textRect.x = startButton.x + (startButton.w - textSurface->w) / 2; //butonun tam ortasina yaziyi yerlestir
+            textRect.y = startButton.y + (startButton.h - textSurface->h) / 2; //butonun tam ortasina yaziyi yerlestir
             textRect.w = textSurface->w; //yazinin genisligi
             textRect.h = textSurface->h; //yazinin yuksekligi
-            SDL_RenderCopy(renderer, textTexture, NULL, &textRect); //yaziyi ekrana cizdirir
+            SDL_RenderCopy(renderer, textTexture, NULL, &textRect); //buton yazisini ekrana cizdirir
+            SDL_FreeSurface(textSurface); //surface'i serbest birakiyoruz cunku artik texture
 
-            SDL_FreeSurface(textSurface); //surface'i serbest birakiyoruz cunku artik texture olarak kullanacagiz
-            SDL_DestroyTexture(textTexture); //text texture'ini yok ediyoruz cunku her dongude yeniden olusturuluyor
         }
+        
+
+        else{
+
+            for (int r = 0; r < ROWS; r++) {
+                for (int c = 0; c < COLS; c++) {
+                    // SDL_Rect, bir dikdortgenin nerede cizilecegini (x,y) koordinatlarini ve boyutunu (w,h) weight height tutan bir yapidir
+                    SDL_Rect rect;
+
+                    rect.x = startX + c * (cardWidth + padding); 
+                    rect.y = startY + r * (cardHeight + padding); 
+                    rect.w = cardWidth;
+                    rect.h = cardHeight;
+
+
+                    if(game.board[r][c].state == 0) {
+                        SDL_SetRenderDrawColor(renderer,118, 206, 242, 255); // Kapali kartlar icin acik mavi
+                        SDL_RenderFillRect(renderer, &rect); // Dikdortgenin icini boyayarak ekrana cizdirir} 
+                    }
+
+                    else if(game.board[r][c].state == 1 || game.board[r][c].state == 2) 
+                    {
+                        int val= game.board[r][c].val; //kart degeri, 1'den 8'e kadar
+
+                        if(cardTexture[val] != NULL) {
+                            //kart degerine karslik gelen texture'i cizdirir
+                            SDL_RenderCopy(renderer, cardTexture[val], NULL, &rect); //kart degerine karslik gelen texture'i cizdirir
+                        }
+                        else {
+                            //eger texture yuklenemedi ise, karti beyaz olarak cizdirir
+                            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Beyaz renk
+                            SDL_RenderFillRect(renderer, &rect); // Dikdortgenin icini boyayarak ekrana cizdirir
+                        }
+                    }
+                }
+            }
+            
+            if(font != NULL){
+                char timeText[100];
+
+                if(game.matchedPairs == 8) {
+                    snprintf(timeText, sizeof(timeText), "Tebrikler! Tum kartlari eslestirdiniz. Kalan sure: %d saniye", game.timeRemaining);
+                }
+                else if(game.timeRemaining <= 0) {
+                    snprintf(timeText, sizeof(timeText), "SURE BITTI! Eslesen kart ciftleri: %d", game.matchedPairs);
+                }
+                else {
+                    snprintf(timeText, sizeof(timeText), "Kalan Sure: %d saniye | Hamle: %d | Eslesen Ciftler: %d", game.timeRemaining, game.moves, game.matchedPairs);
+                }
+
+                SDL_Color textColor = {255, 255, 255}; // Beyaz renk
+                SDL_Surface* textSurface = TTF_RenderText_Solid(font, timeText, textColor); //yaziyi bir surface'e renderlar
+                SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface); //surface'i texture'a cevirir
+
+                SDL_Rect textRect;
+                textRect.x = 20; //yazinin x konumu
+                textRect.y = 20; //yazinin y konumu
+                textRect.w = textSurface->w; //yazinin genisligi
+                textRect.h = textSurface->h; //yazinin yuksekligi
+                SDL_RenderCopy(renderer, textTexture, NULL, &textRect); //yaziyi ekrana cizdirir
+
+                SDL_FreeSurface(textSurface); //surface'i serbest birakiyoruz cunku artik texture olarak kullanacagiz
+                SDL_DestroyTexture(textTexture); //text texture'ini yok ediyoruz cunku her dongude yeniden olusturuluyor
+            }
+        
+            if(game.isGameOver == 1) {
+                //oyun bittiginde restart butonunu cizdir
+
+                SDL_SetRenderDrawColor(renderer, 50, 200, 50, 255); // Yesil renk
+                SDL_RenderFillRect(renderer, &restartButton); //restart butonunu cizdir
+            
+                if(font !=NULL){
+                    SDL_Color buttonTextColor = {255, 255, 255}; // Beyaz renk
+                    SDL_Surface* buttonTextSurface = TTF_RenderText_Solid(font, "Yeniden Baslat", buttonTextColor); //buton yazisini bir surface'e renderlar
+             
+                    if(buttonTextSurface != NULL) {
+                        SDL_Texture* buttonTextTexture = SDL_CreateTextureFromSurface(renderer, buttonTextSurface); //surface'i texture'a cevirir
+
+                        SDL_Rect buttonTextRect;
+                        buttonTextRect.x = restartButton.x + (restartButton.w - buttonTextSurface->w) / 2; //butonun tam ortasina yaziyi yerlestir
+                        buttonTextRect.y = restartButton.y + (restartButton.h - buttonTextSurface->h) / 2;
+                        buttonTextRect.w = buttonTextSurface->w;
+                        buttonTextRect.h = buttonTextSurface->h;
+
+                        SDL_RenderCopy(renderer, buttonTextTexture, NULL, &buttonTextRect); //buton yazisini ekrana cizdirir
+                        SDL_FreeSurface(buttonTextSurface); //surface'i serbest birakiyoruz cunku artik texture olarak kullanacagiz
+                        SDL_DestroyTexture(buttonTextTexture); //buton text texture'ini yok ediyoruz cunku her dongude yeniden olusturuluyor
+                    }
+                }
+   
+            }
+        }
+
         SDL_RenderPresent(renderer); //render edilen her seyi ekrana gosterir
         
     
